16f4fd41b73080b30caf11ce02656d71263defcf
QSVMena <quiezelvillagra@gmail.com>
2025-09-21 17:06:31 -0600

    Backend Basico

---
+++ controllers/userController.js
@@ -0,0 +1,50 @@
+const {response, request} = require('express');
+
+const usuariosGet = (req = request, res = response) => {
+
+    const  query = req.query;
+    
+    res.json({
+        msg: 'get API - controlador',
+        query
+    })
+};
+
+const usuariosPut = (req, res) => {
+    const {id} = req.params;
+
+    res.json({
+        msg: 'put API - controlador',
+        id
+    })
+};
+
+const usuariosPost = (req, res) => {
+
+    const {nombre, edad} = req.body;
+    
+    res.status(201).json({
+        msg: 'post API -  controlador',
+        nombre, edad
+    })
+};
+
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API - controlador',
+    })
+};
+
+ const usuariosPatch = (req, res) => {
+    res.json({
+        msg: 'patch API - controlador'
+    })
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
---
+++ models/server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+
+class Server {
+    constructor() {
+        this.app = express();
+        this.port = process.env.PORT;
+        this.usuariosPath = '/api/usuarios'
+
+        //MIDDLEWARES
+        this.middlewares();
+
+        //Rutas de mi aplicacion
+
+        this.routes();
+    }
+
+    middlewares() {
+        //CORS
+        this.app.use(cors());
+
+        //lectura y parseo del body
+        this.app.use(express.json());
+
+        //
+        this.app.use(express.static('public'))
+    }
+
+    routes() {
+
+        //Pero aqui tenemos un problema, tenemos los controladores y las rutas juntas y hay que separar
+        //mis controladores comienzan desde el (req, res) => {
+        //mi ruta comienza desde  this.app.get('/api',
+
+        this.app.use(this.usuariosPath, require('../routes/user.js')); //se hace como un middleware condicional
+    }
+
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log(`Example app listening on port ${this.port}`)
+        })
+    }
+}
+
+module.exports = Server;
